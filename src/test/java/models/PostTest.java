b94f869f7980e6305516a10652b2d1fa0adf8bd5
Jun Li <june11084@gmail.com>
2018-01-12 14:48:56 -0800

    working method and test for printing arrayList to string

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -4,6 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PostTest {
@@ -21,25 +25,28 @@ public class PostTest {
     }
 
   @Test
-  public void Post_getMember() throws Exception {
+  public void Post_getMember_toString() throws Exception {
     Post post = new Post("Day 1: Intro");
-    assertEquals("Day 1: Intro", post.getTeam());
+    String members ="john,christine,Lee,adam,dave";
+    List<String> membersList = new ArrayList<>(Arrays.asList(members.split(" , ")));
+    post.updateMember((ArrayList) membersList);
+    assertEquals("john,christine,Lee,adam,dave", post.getMembers());
   }
 
 
 
-    @Test
-    public void updateChangesPostContent() throws Exception {
-        Post post = setupNewPost();
-        String formerContent = post.getContent();
-        LocalDateTime formerDate = post.getCreatedAt();
-        int formerId = post.getId();
-
-        post.update("Android: Day 40");
-
-        assertEquals(formerId, post.getId());
-        assertEquals(formerDate, post.getCreatedAt());
-        assertNotEquals(formerContent, post.getContent());
-    }
+//    @Test
+//    public void updateChangesPostContent() throws Exception {
+//        Post post = setupNewPost();
+//        String formerContent = post.getContent();
+//        LocalDateTime formerDate = post.getCreatedAt();
+//        int formerId = post.getId();
+//
+//        post.update("Android: Day 40");
+//
+//        assertEquals(formerId, post.getId());
+//        assertEquals(formerDate, post.getCreatedAt());
+//        assertNotEquals(formerContent, post.getContent());
+//    }
 
 }
