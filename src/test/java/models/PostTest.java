9dd1f94c9c0dcd32ae4921b8b189f24078030b56
Jun Li <june11084@gmail.com>
2018-01-12 15:18:26 -0800

    working test for finding instance by id

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -24,23 +24,33 @@ public class PostTest {
         assertEquals("Day 1: Intro", post.getTeam());
     }
 
-  @Test
-  public void Post_ClearsAllInstances() throws Exception {
-    Post post = new Post("Day 1: Intro");
-    Post post1 = new Post("Day 2: Intro");
-    Post post2 = new Post("Day 3: Intro");
-    Post.clearAllPosts();
-    assertEquals(0, Post.getAll().size() );
-  }
+    @Test
+    public void Post_CanFindById() throws Exception {
+      Post post = new Post("Day 1: Intro");
+      Post post1 = new Post("Day 2: Intro");
+      Post post2 = new Post("Day 3: Intro");
+      assertEquals(Post.getAll().get(0), Post.findById(1));
+      assertEquals(Post.getAll().get(1), Post.findById(2));
+      assertEquals(Post.getAll().get(2), Post.findById(3));
+    }
 
-  @Test
-  public void Post_getMember_toString() throws Exception {
-    Post post = new Post("Day 1: Intro");
-    String members ="john,christine,Lee,adam,dave";
-    List<String> membersList = new ArrayList<>(Arrays.asList(members.split(" , ")));
-    post.updateMember((ArrayList) membersList);
-    assertEquals("john,christine,Lee,adam,dave", post.getMembers());
-  }
+    @Test
+    public void Post_ClearsAllInstances() throws Exception {
+      Post post = new Post("Day 1: Intro");
+      Post post1 = new Post("Day 2: Intro");
+      Post post2 = new Post("Day 3: Intro");
+      Post.clearAllPosts();
+      assertEquals(0, Post.getAll().size() );
+    }
+
+    @Test
+    public void Post_getMember_toString() throws Exception {
+      Post post = new Post("Day 1: Intro");
+      String members ="john,christine,Lee,adam,dave";
+      List<String> membersList = new ArrayList<>(Arrays.asList(members.split(" , ")));
+      post.updateMember((ArrayList) membersList);
+      assertEquals("john,christine,Lee,adam,dave", post.getMembers());
+    }
 
 
 
